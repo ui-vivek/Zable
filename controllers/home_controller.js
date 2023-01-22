54e897e1a79c657b591435744dae127d3b4a764c
vivek singh <cu.vivek.1557@gmail.com>
2023-01-22 12:09:19 +0530

    change to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,21 +1,26 @@
 // module.exports.actionName=function(req,res){----}
 const Post=require('../models/post')
 const User=require('../models/user')
-module.exports.home=function(req,res){
-    Post.find({}).populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    }).exec(function(err, posts){
-        User.find({},function(err,users){
+module.exports.home= async function(req,res){
+    try{
+        const posts= await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+        let users= await User.find({})
             return res.render('home', {
                 title: "Zable | Home",
                 posts: posts,
                 all_users:users
             });
-        })
-        
-    })
+
+    }catch(err){
+        console.log("Error",err);
+        return;
+    }
+
 };
