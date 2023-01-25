a8f9c8075ec8faab62395aff88e13d6bf43c8011
vivek singh <cu.vivek.1557@gmail.com>
2023-01-25 14:14:41 +0530

    Adding JSON

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,6 @@ router.use('/comments',require('./comments'))
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
+router.use('/api',require('./api'))
+
 module.exports=router;
