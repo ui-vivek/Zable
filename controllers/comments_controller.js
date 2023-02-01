eda65e6f2a029a0b07a7990152a226ad3e89b4ad
vivek singh <cu.vivek.1557@gmail.com>
2023-02-01 22:31:13 +0530

    adding parallel jobs using kue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer=require('../mailers/comments_mailer')
+const queue=require('../config/kue')
+const commentEmailWorker=require('../workers/comment_email_worker')
 
 module.exports.create = async function(req, res){
 
@@ -19,7 +21,14 @@ module.exports.create = async function(req, res){
 
             // comment = await comment.populate('user', 'name').execPopulate();
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job=queue.create('emails',comment).save(function(err){
+                if(err){
+                    console.log("Error in creating a queue",err);
+                    return;
+                }
+                console.log('job enqued',job.id)
+            })
             if (req.xhr){
                 // Similar for comments to fetch the user's id!
     
